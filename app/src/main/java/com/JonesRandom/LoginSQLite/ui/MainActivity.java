8c9fb764286ecbe6f5871e6618cad46d33ee78ad
jonesrandom <jones.random97@gmail.com>
2017-11-27 20:32:34 +0800

    Rename Class

---
+++ app/src/main/java/com/JonesRandom/LoginSQLite/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.JonesRandom.LoginSQLite.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.JonesRandom.LoginSQLite.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user);
+
+    }
+}
---
+++ app/src/main/java/com/JonesRandom/LoginSQLite/ui/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package com.JonesRandom.LoginSQLite.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
