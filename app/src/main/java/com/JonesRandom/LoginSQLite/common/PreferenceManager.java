dd7dca2fe1cb5408fcb951e3f791a058bb636ab9
jonesrandom <jones.random97@gmail.com>
2017-11-28 21:57:08 +0800

    update PreferenceManager

--- app/src/main/java/com/JonesRandom/LoginSQLite/common/PreferenceManager.java
+++ app/src/main/java/com/JonesRandom/LoginSQLite/common/PreferenceManager.java
@@ -21,8 +21,8 @@ public class PreferenceManager {
         preferences = context.getSharedPreferences("PREF_MANAGER", Context.MODE_PRIVATE);
     }
 
-    public static PreferenceManager initPreferences(Context context) {
-        return new PreferenceManager(context);
+    public static void initPreferences(Context context) {
+        new PreferenceManager(context);
     }
 
     public static boolean isUserlogged() {
@@ -33,21 +33,21 @@ public class PreferenceManager {
 
         ModelUser user = new ModelUser();
         user.userID = preferences.getInt(DatabaseConstan.ROW_ID, 0);
-        user.user = preferences.getString(DatabaseConstan.ROW_USER, "user");
+        user.username = preferences.getString(DatabaseConstan.ROW_USER, "user");
         user.email = preferences.getString(DatabaseConstan.ROW_EMAIL, "email");
         user.password = preferences.getString(DatabaseConstan.ROW_PASSWORD, "password");
         return user;
     }
 
     public static void userLogout() {
-        preferences.edit().putBoolean(PREF_LOGIN, false).apply();
+        preferences.edit().clear().apply();
     }
 
     public static void userLogin(ModelUser user) {
         SharedPreferences.Editor editor = preferences.edit();
-        editor.putInt(DatabaseConstan.ROW_ID, user.userID);
+        editor.putInt(DatabaseConstan.ROW_ID, user.getUserID());
         editor.putString(DatabaseConstan.ROW_EMAIL, user.getEmail());
-        editor.putString(DatabaseConstan.ROW_USER, user.getPassword());
+        editor.putString(DatabaseConstan.ROW_USER, user.getUser());
         editor.putString(DatabaseConstan.ROW_PASSWORD, user.getPassword());
         editor.putBoolean(PREF_LOGIN, true);
         editor.apply();
