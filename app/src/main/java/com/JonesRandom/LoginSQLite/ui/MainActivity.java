7db36513be66e136471d49119d581d03c8ee2cbf
jonesrandom <jones.random97@gmail.com>
2017-11-30 20:11:31 +0800

    add sign out

--- app/src/main/java/com/JonesRandom/LoginSQLite/ui/MainActivity.java
+++ app/src/main/java/com/JonesRandom/LoginSQLite/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.JonesRandom.LoginSQLite.ui;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,6 +13,7 @@ import com.JonesRandom.LoginSQLite.model.ModelUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +32,26 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.textUserHeader)
     TextView textUserHeader;
 
+    @OnClick(R.id.btnSignOut)
+    public void signOut() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Hai " + user.getUser());
+        builder.setMessage("Apakah Kamu Ingin Keluar ?");
+        builder.setPositiveButton("KELUAR", (dialogInterface, i) -> {
+
+            PreferenceManager.userLogout();
+            startActivity(new Intent(MainActivity.this , AuthActivity.class));
+            finish();
+
+        });
+        builder.setNegativeButton("BATAL", null);
+        builder.create().show();
+
+    }
+
+    ModelUser user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +66,10 @@ public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("SetTextI18n")
     private void bindView() {
-        ModelUser user = PreferenceManager.getUser();
+        user = PreferenceManager.getUser();
 
         textDetailUser.setText(user.getUser());
-        textDetailUserID.setText("ID "+user.getUserID());
+        textDetailUserID.setText("ID " + user.getUserID());
         textDetailEmail.setText(user.getEmail());
         textDetailPassword.setText(user.getPassword());
 
